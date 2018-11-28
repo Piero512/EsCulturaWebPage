275a5a1a6d0efc54cafd03674601a1b9801fd810
piero512 <ulloapiero@gmail.com>
2018-11-28 10:21:00 -0500

    Añadido páginas faltantes a AngularJS router

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,15 @@ var app = angular.module('esCultura', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
-
+  .when('/',{
+    templateUrl: 'pages/home.html'
+  })
   .when('/about', {
     templateUrl : 'pages/about.html'
   })
-
+  .when('/what_we_do',{
+    templateUrl : 'pages/what_we_do.html'
+  })
   .when('/contact', {
     templateUrl : 'pages/contact_us.html'
   })
